08afeb9a14cab636a7ca63cd8aeb6695c33104fb
Nadyan <nadyan_sp@hotmail.com>
2017-08-17 21:43:35 -0300

    scripts

---
+++ shell/tempoVLC.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Script shell para marcar o tempo de execucao do comando
+# de stream do VLC
+
+TEMPO1=$(($(date +%s%3N))) # tempo em ms
+
+# comando pra executar o vlc:
+vlc -vvv video.mp4 --sout '#rtp{mux=ts,dst=10.0.0.2,sdp=sap,name="teste"}'
+
+TEMPO2=$(($(date +%s%3N)))
+
+TEMPO=$TEMPO2-$TEMPO1
+
+echo $TEMPO3 ms
