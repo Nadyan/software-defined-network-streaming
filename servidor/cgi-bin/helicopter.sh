fb1b90b644e93fb06eab4a8fac56db2b4c4de5a1
Nadyan <nadyan_sp@hotmail.com>
2017-10-11 01:50:17 -0300

    scripts de execucao atualizados

--- servidor/cgi-bin/helicopter.sh
+++ servidor/cgi-bin/helicopter.sh
@@ -2,20 +2,10 @@
 
 # Nadyan Suriel Pscheidt - UDESC
 # Abordagem para Distribuição de Vídeo Baseada em Redes Definidas por Software
-
-# IP do Cliente requisitante
-IPCLI=$REMOTE_ADDR
-NOMECLI=$REMOTE_USER
+# Script para a transmissão do vídeo helicopter.divx
 
 # Cria a transmissão através do VLC utilizando RTP
 COMANDO="vlc -vvv videos/helicopter.divx --sout '#rtp{mux=ts,dst='$IPCLI',sdp=sap}'"
 
 # Executa
 eval $COMANDO
-
-echo "Content-type: text/html"
-echo ""
-echo "<html><head><title>Helicopter.divx</title></head></body>"
-echo "Video Helicopter.divx sendo enviado para o cliente $NOMECLI com IP $IPCLI"
-echo "</body></html>"
-
