30c3fff2cdb7c632d4bebacea3d27cc0820b0a69
Nadyan <nadyan_sp@hotmail.com>
2017-10-11 01:56:43 -0300

    att scripts

--- shell/coelho.sh
+++ shell/coelho.sh
@@ -2,19 +2,10 @@
 
 # Nadyan Suriel Pscheidt - UDESC
 # Abordagem para Distribuição de Vídeo Baseada em Redes Definidas por Software
+# Script para a tranmissão do vídeo coelho.sh
 
-# IP do Cliente requisitante
-IPCLI=$REMOTE_ADDR
-NOMECLI=$REMOTE_USER
+# Cria a transmissão através do VLC utilizando RTP 
+COMANDO="vlc -vvv videos/video.mp4 --sout '#rtp{mux=ts,dst='$REMOTE_ADDR',sdp=sap}'"
 
-# Cria a transmissão através do VLC utilizando RTP
-COMANDO="sudo vlc -vvv video.mp4 --sout '#rtp{mux=ts,dst='$IPCLI',sdp=sap}'"
-
-# Executa
+# Executa a transmissão
 eval $COMANDO
-
-echo "Content-type: text/html"
-echo ""
-echo "<html><head><title>Coelho.mp4</title></head></body>"
-echo "Video Coelho.mp4 sendo enviado para o cliente $NOMECLI com IP $IPCLI"
-echo "</body></html>"
